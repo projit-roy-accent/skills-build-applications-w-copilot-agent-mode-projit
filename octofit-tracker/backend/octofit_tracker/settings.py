7c0a8b307adda55a1c92184a685fee01a161d23b
projit-roy-accent <projit.roy@accenture.com>
2026-02-17 09:50:22 +0000

    Backend execution

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-hygc7qkuf=j%1a!+ftdja$7d&oa6t94-(z#ku#gg#+q*2bx(2i
 DEBUG = True
 
 
-# Allow all hosts for development
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
